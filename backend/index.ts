619d458025f74b5e88887af49f48644ba8ee417f
exchange-wata <exchange.wata0907@gmail.com>
2023-11-20 08:30:11 +0900

    typeへ変更とコメント追加

--- backend/index.ts
+++ backend/index.ts
@@ -3,23 +3,33 @@ import fetch from "node-fetch"
 
 const GOOGLE_BOOKS_API_URL = 'https://www.googleapis.com/books/v1/volumes';
 
-interface Book {
+type Book = {
   title: string;
   authors: string[];
   description: string;
+  publishedDate: string;
+  selfLink: string;
+  pageCount: number;
 }
 
 async function getBookInfoByTitle(title: string): Promise<Book | null> {
   try {
+    // TODO: スペース区切り文字のformat
+    // TODO: 複数語句での検索は「+」で繋いでいく
     const response = await fetch(`${GOOGLE_BOOKS_API_URL}?q=${encodeURIComponent(title)}`);
     const data = await response.json();
+    console.log('=================================')
+    console.log(data.items)
 
     if (data.items && data.items.length > 0) {
       const bookData = data.items[0].volumeInfo;
       const book: Book = {
         title: bookData.title,
         authors: bookData.authors || [],
-        description: bookData.description || 'No description available.'
+        description: bookData.description || 'No description available.',
+        publishedDate: "",
+        selfLink: "",
+        pageCount: 0
       };
       return book;
     }
@@ -34,8 +44,6 @@ async function getBookInfoByTitle(title: string): Promise<Book | null> {
 async function main() {
   const bookTitle = 'ノルウェイ'; // お好きな書名に変更してください
   const bookInfo = await getBookInfoByTitle(bookTitle);
-  console.log('=================================')
-  console.log(bookInfo)
 
   if (bookInfo) {
     console.log('Book Information:');
