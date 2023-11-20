eb9d29b76723827165007d683459513d49973a47
exchange-wata <exchange.wata0907@gmail.com>
2023-11-20 11:42:59 +0900

    メソッド分割&package.json修正

--- backend/index.ts
+++ backend/index.ts
@@ -5,54 +5,53 @@ const GOOGLE_BOOKS_API_URL = 'https://www.googleapis.com/books/v1/volumes';
 
 type Book = {
   title: string;
-  authors: string[];
-  description: string;
-  publishedDate: string;
-  selfLink: string;
-  pageCount: number;
+  authors: string[]
 }
 
-async function getBookInfoByTitle(title: string): Promise<Book | null> {
+// TODO: スペース区切り文字のformat→スペースの種類対応が必須かも？
+// TODO: 複数語句での検索は「+」で繋いでいく
+const createQueryParam = (param: string): string => param.replace(' ', '+')
+
+// データ取得のみ
+const getBookInfoByTitle = async (queryParams: string): Promise<Book | null> => {
   try {
-    // TODO: スペース区切り文字のformat
-    // TODO: 複数語句での検索は「+」で繋いでいく
-    const response = await fetch(`${GOOGLE_BOOKS_API_URL}?q=${encodeURIComponent(title)}`);
-    const data = await response.json();
-    console.log('=================================')
-    console.log(data.items)
-
-    if (data.items && data.items.length > 0) {
-      const bookData = data.items[0].volumeInfo;
-      const book: Book = {
-        title: bookData.title,
-        authors: bookData.authors || [],
-        description: bookData.description || 'No description available.',
-        publishedDate: "",
-        selfLink: "",
-        pageCount: 0
-      };
-      return book;
-    }
-
-    return null;
+    const response = await fetch(`${GOOGLE_BOOKS_API_URL}?q=${encodeURIComponent(queryParams)}`);
+    return response.json();
   } catch (err) {
     console.error('Error fetching book information:', err);
     return null;
   }
 }
 
-async function main() {
-  const bookTitle = 'ノルウェイ'; // お好きな書名に変更してください
-  const bookInfo = await getBookInfoByTitle(bookTitle);
+// データ加工のみ
+// TODO: 引数の型
+const format = async (data: any) => {
+  const allBookInfo = data.items;
+  if (allBookInfo && allBookInfo.length > 0) {
+    return allBookInfo.map((each: any) => {
+      const bookInfo = each.volumeInfo;
+      return ({
+        title: bookInfo.title,
+        authors: [...bookInfo.authors] || []
+      })
+  })
+}}
 
-  if (bookInfo) {
-    console.log('Book Information:');
-    console.log('Title:', bookInfo.title);
-    console.log('Authors:', bookInfo.authors.join(', '));
-    console.log('Description:', bookInfo.description);
-  } else {
-    console.log('Book not found.');
-  }
+export async function main() {
+  const bookTitle = '江國'; // お好きな書名に変更してください
+  const queryParams = createQueryParam(bookTitle)
+  const bookInfo = await getBookInfoByTitle(queryParams);
+  console.log(format(bookInfo))
+  return format(bookInfo);
+
+  // if (bookInfo) {
+  //   console.log('Book Information:');
+  //   console.log('Title:', bookInfo.title);
+  //   console.log('Authors:', bookInfo.authors.join(', '));
+  //   console.log('Description:', bookInfo.description);
+  // } else {
+  //   console.log('Book not found.');
+  // }
 }
 
 main();
